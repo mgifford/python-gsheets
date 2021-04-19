d3cd5fdd15c23b84f7392115256347ef9c91e7f8
Mike Gifford <mike.gifford@civicactions.com>
2021-04-19 18:50:01 -0400

    Adding more columns & inline docs
    wanting to make sure that the readability elements are well documented.

--- domains.py
+++ domains.py
@@ -61,6 +61,7 @@ for x in range(cfg.start, cfg.end):
         worksheet = sh.worksheet("shell") # By title
         downloaded = trafilatura.fetch_url(url)
         worksheet.update("B" + row, len(str(downloaded)))
+#        print(downloaded)
 
 #    print(downloaded)
     if url != 0 and downloaded:
@@ -70,21 +71,60 @@ for x in range(cfg.start, cfg.end):
         readable_article = Document(downloaded).summary()
 
         raw = html2text(readable_article)
-#        print(raw)
-        worksheet = sh.worksheet("shell") # By title
-        freading = textstat.flesch_reading_ease(raw)
-        worksheet.update("C" + row, freading)
+        print(raw)
 
-        worksheet = sh.worksheet("shell") # By title
-        smog_index = textstat.smog_index(raw)
-        worksheet.update("D" + row, smog_index)
+        # Lexicon Count - number of words present in the text
+        lexicon_count = textstat.lexicon_count(raw, removepunct=True)
+        worksheet.update("C" + row, lexicon_count)
 
-        worksheet = sh.worksheet("shell") # By title
+        # Sentence Count
+        sentence_count = textstat.sentence_count(raw)
+        worksheet.update("D" + row, sentence_count)
+
+        # The Flesch Reading Ease formula
+        # 90-100 - Very Easy | 80-89 - Easy | 70-79 - Fairly Easy | 60-69 - Standard
+        # 50-59 - Fairly Difficult | 30-49 - Difficult | 0-29 - Very Confusing
+        flesch_reading_ease = textstat.flesch_reading_ease(raw)
+        worksheet.update("E" + row, flesch_reading_ease)
+
+        # Flesch-Kincaid Grade Level
+        # https://en.wikipedia.org/wiki/Flesch%E2%80%93Kincaid_readability_tests#Flesch%E2%80%93Kincaid_grade_level
         flesch_kincaid_grade = textstat.flesch_kincaid_grade(raw)
-        worksheet.update("E" + row, flesch_kincaid_grade)
+        worksheet.update("F" + row, flesch_kincaid_grade)
 
-        worksheet = sh.worksheet("shell") # By title
+        # The Fog Scale (Gunning FOG Formula)
+        # https://en.wikipedia.org/wiki/Gunning_fog_index
+        gunning_fog = textstat.gunning_fog(raw)
+        worksheet.update("G" + row, gunning_fog)
+
+        # The SMOG Index
+        # https://en.wikipedia.org/wiki/SMOG
+        smog_index = textstat.smog_index(raw)
+        worksheet.update("H" + row, smog_index)
+
+        # Automated Readability Index
+        # https://en.wikipedia.org/wiki/Automated_readability_index
         automated_readability_index = textstat.automated_readability_index(raw)
-        worksheet.update("F" + row, automated_readability_index)
+        worksheet.update("I" + row, automated_readability_index)
 
-#        print(freading)
+        # The Coleman-Liau Index
+        # https://en.wikipedia.org/wiki/Coleman%E2%80%93Liau_index
+        coleman_liau_index = textstat.coleman_liau_index(raw)
+        worksheet.update("J" + row, coleman_liau_index)
+
+        # Linsear Write Formula
+        # https://en.wikipedia.org/wiki/Linsear_Write
+        linsear_write_formula = textstat.linsear_write_formula(raw)
+        worksheet.update("K" + row, linsear_write_formula)
+
+        # Dale-Chall Readability Score
+        # < 4.9 - average 4th-grade student | 5.0–5.9 - average 5th or 6th-grade
+        # 6.0–6.9 - average 7th or 8th-grade | 7.0–7.9 - average 9th or 10th-grade
+        # 8.0–8.9	average 11th or 12th-grade | 9.0–9.9 - college student
+        dale_chall_readability_score = textstat.dale_chall_readability_score(raw)
+        worksheet.update("L" + row, dale_chall_readability_score)
+
+        # Readability Consensus based upon all the above tests
+        # Estimated school grade level required to understand the text
+        text_standard = textstat.text_standard(raw, float_output=False)
+        worksheet.update("M" + row, text_standard)
